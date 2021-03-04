9a7532d1695fc3d10dfee7a928859a86b2768da7
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-03-04 16:04:53 +0100

    Database/Ignores

--- e_siwes/settings.py
+++ e_siwes/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'e_siwes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'e_siwes',
+        'HOST': '127.0.0.1',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT': 3306
     }
 }
 
